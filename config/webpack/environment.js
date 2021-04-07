cffe7a1169db6d61cc7415d93c166f8694d851bb
malikshoaib715 <malik.shoaib715@gmail.com>
2021-04-07 17:04:33 +0500

    first commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,8 @@
+// const { environment } = require('@rails/webpacker')
+
+const { resolve } = require('path');
+const { config, environment, Environment } = require('@rails/webpacker');
+const WebpackerPwa = require('webpacker-pwa');
+new WebpackerPwa(config, environment);
+
+module.exports = environment
