e843ebb0d8b6fff7fda361ab5033b68c0856672e
malikshoaib715 <malik.shoaib715@gmail.com>
2021-04-12 13:44:19 +0500

    asking for notification permission

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,23 +11,43 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+//
+// window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//         console.log('ServiceWorker registered: ', registration);
+//
+//     //     var serviceWorker;
+//     //     if (registration.installing) {
+//     //         serviceWorker = registration.installing;
+//     //         console.log('Service worker installing.');
+//     //     } else if (registration.waiting) {
+//     //         serviceWorker = registration.waiting;
+//     //         console.log('Service worker installed & waiting.');
+//     //     } else if (registration.active) {
+//     //         serviceWorker = registration.active;
+//     //         console.log('Service worker active.');
+//     //     }
+//     // }).catch(registrationError => {
+//     //     console.log('Service worker registration failed: ', registrationError);
+//     // });
+//
+//
+//         window.Notification.requestPermission().then(permission => {
+//             if(permission !== 'granted'){
+//                 throw new Error('Permission not granted for Notification');
+//             }
+//         });
+//     });
+// });
 
-window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-        console.log('ServiceWorker registered: ', registration);
+navigator.serviceWorker.register('/service-worker.js').then(registration => {
+    console.log('ServiceWorker registered: ', registration);
 
-        var serviceWorker;
-        if (registration.installing) {
-            serviceWorker = registration.installing;
-            console.log('Service worker installing.');
-        } else if (registration.waiting) {
-            serviceWorker = registration.waiting;
-            console.log('Service worker installed & waiting.');
-        } else if (registration.active) {
-            serviceWorker = registration.active;
-            console.log('Service worker active.');
+    // all code from before
+
+    window.Notification.requestPermission().then(permission => {
+        if(permission !== 'granted'){
+            throw new Error('Permission not granted for Notification');
         }
-    }).catch(registrationError => {
-        console.log('Service worker registration failed: ', registrationError);
     });
 });
